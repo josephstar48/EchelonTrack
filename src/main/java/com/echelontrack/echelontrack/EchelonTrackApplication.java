f86df0fd9919bd4863fdc925853086367af0eb73
Jose R. Estrella Sr <josephstar48@hotmail.com>
2025-03-24 13:36:36 -0400

    testing

--- src/main/java/com/echelontrack/echelontrack/EchelonTrackApplication.java
+++ src/main/java/com/echelontrack/echelontrack/EchelonTrackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EchelonTrackApplication {
-// app entry point
+
 	public static void main(String[] args) {
 		SpringApplication.run(EchelonTrackApplication.class, args);
 	}
