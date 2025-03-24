68fb3b5a27539dd0569702c2cbb8f5202822d049
Jose R. Estrella Sr <joestar48@Joses-MacBook-Pro.local>
2025-03-24 12:52:13 -0400

    Initial commit

---
+++ src/main/java/com/echelontrack/echelontrack/EchelonTrackApplication.java
@@ -0,0 +1,13 @@
+package com.echelontrack.echelontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchelonTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchelonTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echelontrack/echelontrack/EchelonTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echelontrack.echelontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchelonTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
